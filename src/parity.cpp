5f472e390bbf531dfe77ed978c5b6b0d5a52fc45
TTifanioh <167166956+TTifanioh@users.noreply.github.com>
2025-08-21 11:50:59 +0300

    Exercice : Parity

---
+++ src/parity.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int nb;
+  cout << "Enter a first integer :" ;
+  cin >> nb;
+  if ( nb % 2 == 0){ cout << " This number is pair"}
+  else {cout << "This number is impair"}
+  return 0;
+}
