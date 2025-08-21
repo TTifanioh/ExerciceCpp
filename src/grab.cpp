9d5e7745ec1b1fc25c25b686ef549ae530099ade
TTifanioh <167166956+TTifanioh@users.noreply.github.com>
2025-08-21 11:36:37 +0300

    Exercice : To grab note

---
+++ src/grab.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double nt1,nt2;
+  cout  << "Enter the note 1:";
+  cin  >> nt1;
+  cout  << "Enter the note 2:";
+  cin  >> nt2;
+  cout << "Note 1:" << nt1;
+  cout << "Note 2:" << nt2;
+  return 0;
+}
