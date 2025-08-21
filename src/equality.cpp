8d5a76c514879dbb2f0331d5a9db9331fc0a8a55
TTifanioh <167166956+TTifanioh@users.noreply.github.com>
2025-08-21 11:45:04 +0300

    Exercice : Equality

---
+++ src/equality.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int nb1,nb2;
+  cout << "Enter a first integer :" ;
+  cin >> nb1;
+   cout << "Enter a second integer :" ;
+  cin >> nb2;
+  if (nb1 == nb2){ cout << " This is equal." ;}
+  else { cout << " This is different.";}
+  return 0;
+}
